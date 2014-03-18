21959b6e08c8fc5bcc4401fd0d252f45d8ee122a
Tanner Danzey <arkaniad@gmail.com>
2014-03-18 18:10:04 -0500

    Testing sprite engine failed.

--- Galaga Clone/src/com/tanner/galaga/Core.java
+++ Galaga Clone/src/com/tanner/galaga/Core.java
@@ -13,7 +13,7 @@ public class Core {
 	private final int y_res = 600;
 	private final String title = "Tanner's Galaga Clone";
 	private long last_time;
-	
+
 	// Game variables
 	float xpos = 400;
 	float ypos = 400;
@@ -21,9 +21,12 @@ public class Core {
 	long lastFrame;
 	int fps;
 	long lastFPS;
-	
+	private Sprite test_sprite;
+	private TextureLoader textureLoader;
+
 	/**
-	 * @param args Command line arguments.
+	 * @param args
+	 *            Command line arguments.
 	 */
 	public void start() {
 		// Initialization
@@ -31,8 +34,10 @@ public class Core {
 		initDisplay();
 		initGL();
 		initTime();
-		
-		while(!Display.isCloseRequested()) {
+		initTexture();
+		initSprite();
+
+		while (!Display.isCloseRequested()) {
 			int delta = getDelta();
 			update(delta);
 			renderGL();
@@ -42,21 +47,22 @@ public class Core {
 		log.dbg("Closing.");
 		Display.destroy();
 	}
-	
+
 	private void pollInput() {
 		while (Keyboard.next()) {
-			if(Keyboard.getEventKeyState()) {
-				if(Keyboard.getEventKey() == Keyboard.KEY_SPACE) {
+			if (Keyboard.getEventKeyState()) {
+				if (Keyboard.getEventKey() == Keyboard.KEY_SPACE) {
 					log.dbg("Key pressed: SPACE");
 				}
 			}
 		}
 	}
+
 	private void initLogger() {
 		log = new Logger("[Galaga]", System.out);
 		log.dbg("Logging initialized.");
 	}
-	
+
 	private void initDisplay() {
 		log.info("Initializing screen subsystem...");
 		try {
@@ -69,35 +75,53 @@ public class Core {
 			System.exit(1);
 		}
 	}
-	
+
 	private void initGL() {
+		log.info("Initializing OpenGL");
 		GL11.glMatrixMode(GL11.GL_PROJECTION);
 		GL11.glLoadIdentity();
 		GL11.glOrtho(0, 800, 0, 600, 1, -1);
 		GL11.glMatrixMode(GL11.GL_MODELVIEW);
 	}
-	
+
 	private void initTime() {
 		lastFPS = getTime();
 	}
 	
+	private void initTexture() {
+		log.info("Initializing texture engine..");
+		textureLoader = new TextureLoader();
+	}
+	private void initSprite() {
+		log.info("Initializing test sprite.");
+		test_sprite = new Sprite(textureLoader, "test.png");
+	}
+
 	private void update(int delta) {
 		rotation += 0.15f * delta;
-		if(Keyboard.isKeyDown(Keyboard.KEY_LEFT)) xpos -= 0.35f * delta;
-		if(Keyboard.isKeyDown(Keyboard.KEY_RIGHT)) xpos += 0.35f * delta;
-		
-		if(Keyboard.isKeyDown(Keyboard.KEY_UP)) ypos += 0.35f * delta;
-		if(Keyboard.isKeyDown(Keyboard.KEY_DOWN)) ypos -= 0.35f * delta;
-		
+		if (Keyboard.isKeyDown(Keyboard.KEY_LEFT))
+			xpos -= 0.35f * delta;
+		if (Keyboard.isKeyDown(Keyboard.KEY_RIGHT))
+			xpos += 0.35f * delta;
+
+		if (Keyboard.isKeyDown(Keyboard.KEY_UP))
+			ypos += 0.35f * delta;
+		if (Keyboard.isKeyDown(Keyboard.KEY_DOWN))
+			ypos -= 0.35f * delta;
+
 		// Keep quad on screen
-		if (xpos < 0) xpos = 0;
-		if (xpos > x_res) xpos = x_res;
-		if (ypos > y_res) ypos = y_res;
-		if (ypos < 0) ypos = 0;
-		
+		if (xpos < 0)
+			xpos = 0;
+		if (xpos > x_res)
+			xpos = x_res;
+		if (ypos > y_res)
+			ypos = y_res;
+		if (ypos < 0)
+			ypos = 0;
+
 		updateFPS();
 	}
-	
+
 	public void updateFPS() {
 		if (getTime() - lastFPS > 1000) {
 			Display.setTitle(title + " | " + "FPS: " + fps);
@@ -106,35 +130,40 @@ public class Core {
 		}
 		fps++;
 	}
+
 	private void renderGL() {
 		clearGL();
-		drawQuad();
+		test_sprite.draw((int) xpos, (int) ypos);
+		//drawQuad();
 	}
+
 	private void clearGL() {
 		GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
 	}
-	
+
 	private void drawQuad() {
 		// TODO adapt to suit needs
-		GL11.glColor3f(0.5f,  0.5f,  1.0f);
-		
+		GL11.glColor3f(0.5f, 0.5f, 1.0f);
+
 		GL11.glPushMatrix();
-			GL11.glTranslatef(xpos,  ypos, 0);
-			GL11.glRotatef(rotation, 0f, 0f, 1f);
-			GL11.glTranslatef(-xpos, -ypos, 0);
-			
-			GL11.glBegin(GL11.GL_QUADS);
-				GL11.glVertex2f(xpos - 50, ypos - 50);
-				GL11.glVertex2f(xpos + 50, ypos - 50);
-				GL11.glVertex2f(xpos + 50, ypos + 50);
-				GL11.glVertex2f(xpos - 50, ypos + 50);
-			GL11.glEnd();
-			
-			GL11.glPopMatrix();
+		GL11.glTranslatef(xpos, ypos, 0);
+		GL11.glRotatef(rotation, 0f, 0f, 1f);
+		GL11.glTranslatef(-xpos, -ypos, 0);
+
+		GL11.glBegin(GL11.GL_QUADS);
+		GL11.glVertex2f(xpos - 50, ypos - 50);
+		GL11.glVertex2f(xpos + 50, ypos - 50);
+		GL11.glVertex2f(xpos + 50, ypos + 50);
+		GL11.glVertex2f(xpos - 50, ypos + 50);
+		GL11.glEnd();
+
+		GL11.glPopMatrix();
 	}
+
 	public long getTime() {
 		return (Sys.getTime() * 1000) / Sys.getTimerResolution();
 	}
+
 	public int getDelta() {
 		long time = getTime();
 		int delta = (int) (time - last_time);
--- Galaga Clone/src/com/tanner/galaga/Texture.java
+++ Galaga Clone/src/com/tanner/galaga/Texture.java
@@ -1,128 +1,187 @@
+/*
+ * Copyright (c) 2002-2010 LWJGL Project
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'LWJGL' nor the names of
+ *   its contributors may be used to endorse or promote products derived
+ *   from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package com.tanner.galaga;
-import org.lwjgl.opengl.GL11;
+
+import static org.lwjgl.opengl.GL11.*;
 
 /**
- * Defines a texture. 
- * Borrowed from LWGL SpaceInvaders example, all license retained therein.
- * @author Tanner Danzey <arkaiad@gmail.com>
- *
+ * A texture to be bound within OpenGL. This object is responsible for keeping
+ * track of a given OpenGL texture and for calculating the texturing mapping
+ * coordinates of the full image.
+ * 
+ * Since textures need to be powers of 2 the actual texture may be considerably
+ * bigged that the source image and hence the texture mapping coordinates need
+ * to be adjusted to matchup drawing the sprite against the texture.
+ * 
+ * @author Kevin Glass
+ * @author Brian Matzon
  */
 public class Texture {
-	// GL Target Type
+
+	/** The GL target type */
 	private int target;
-	// GL Texture ID
+
+	/** The GL texture ID */
 	private int textureID;
-	// Image dimensions
+
+	/** The height of the image */
 	private int height;
+
+	/** The width of the image */
 	private int width;
-	
-	// Texture dimensions
+
+	/** The width of the texture */
 	private int texWidth;
+
+	/** The height of the texture */
 	private int texHeight;
-	
-	// Image -> Texture ratios
+
+	/** The ratio of the width of the image to the texture */
 	private float widthRatio;
+
+	/** The ratio of the height of the image to the texture */
 	private float heightRatio;
-	
+
 	/**
-	 * Create a new texture.
+	 * Create a new texture
 	 * 
-	 * @param target the GL target
-	 * @param textureID the GL texture ID
+	 * @param target
+	 *            The GL target
+	 * @param textureID
+	 *            The GL texture ID
 	 */
 	public Texture(int target, int textureID) {
 		this.target = target;
 		this.textureID = textureID;
 	}
-	
+
 	/**
-	 * Bind specified GL context to texture
+	 * Bind the specified GL context to a texture
 	 */
 	public void bind() {
-		GL11.glBindTexture(target, textureID);
+		glBindTexture(target, textureID);
 	}
-	
+
 	/**
-	 * Set width of image.
+	 * Set the height of the image
 	 * 
-	 * @param width The width of the image
-	 */
-	public void setWidth(int width) {
-		this.width = width;
-		setWidth();
-	}
-	
-	/**
-	 * Set height of image.
-	 * 
-	 * @param height The height of the image
+	 * @param height
+	 *            The height of the image
 	 */
 	public void setHeight(int height) {
 		this.height = height;
 		setHeight();
 	}
-	
+
 	/**
-	 * Get image height.
-	 * @return Height of original image
+	 * Set the width of the image
+	 * 
+	 * @param width
+	 *            The width of the image
 	 */
-	public float getImageHeight() {
+	public void setWidth(int width) {
+		this.width = width;
+		setWidth();
+	}
+
+	/**
+	 * Get the height of the original image
+	 * 
+	 * @return The height of the original image
+	 */
+	public int getImageHeight() {
 		return height;
 	}
-	
+
 	/**
-	 * Get image width.
-	 * @return Width of original image
+	 * Get the width of the original image
+	 * 
+	 * @return The width of the original image
 	 */
-	public float getImageWidth() {
+	public int getImageWidth() {
 		return width;
 	}
-	
+
 	/**
-	 * Get height of physical texture.
-	 * @return Height of physical texture
+	 * Get the height of the physical texture
+	 * 
+	 * @return The height of physical texture
 	 */
 	public float getHeight() {
 		return heightRatio;
 	}
-	
+
 	/**
-	 * Get width of physical texture.
-	 * @return Width of physical texture
+	 * Get the width of the physical texture
+	 * 
+	 * @return The width of physical texture
 	 */
 	public float getWidth() {
 		return widthRatio;
 	}
-	
+
 	/**
-	 * Set height of texture
-	 * @param texHeight The height of the texture
+	 * Set the height of this texture
+	 * 
+	 * @param texHeight
+	 *            The height of the texture
 	 */
 	public void setTextureHeight(int texHeight) {
 		this.texHeight = texHeight;
 		setHeight();
 	}
-	
+
 	/**
-	 * Set width of texture
-	 * @param texWidth The width of the texture
+	 * Set the width of this texture
+	 * 
+	 * @param texWidth
+	 *            The width of the texture
 	 */
 	public void setTextureWidth(int texWidth) {
 		this.texWidth = texWidth;
 		setWidth();
 	}
-	
+
 	/**
-	 * Set height of texture. Updates ratio as well.
+	 * Set the height of the texture. This will update the ratio also.
 	 */
 	private void setHeight() {
 		if (texHeight != 0) {
 			heightRatio = ((float) height) / texHeight;
 		}
 	}
-	
+
 	/**
-	 * Set width of texture. Updates ratio as well.
+	 * Set the width of the texture. This will update the ratio also.
 	 */
 	private void setWidth() {
 		if (texWidth != 0) {
